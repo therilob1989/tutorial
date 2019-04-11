622dcae91458342db36dd9b067c2e42b061bc19e
rilob <richard.lopezbasurto@gmail.com>
2019-04-11 15:18:14 -0500

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireModule } from "@angular/fire";
+import { FormsModule } from "@angular/forms";
+import { environment } from '../environments/environment';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { EmployeeService } from './shared/employee.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+    ],
+  providers: [EmployeeService],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+
+    ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/employee.model.ts
@@ -0,0 +1,10 @@
+
+    export class Employee {
+        id: string;
+        fullName: string;
+        empCode: string;
+        position: string;
+        mobile: string;
+        correo: string;
+    
+}
